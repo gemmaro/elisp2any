43427f19999b052085ade35423addbefa825e24e
gemmaro <gemmaro.dev@gmail.com>
2023-11-25 16:13:17 +0900

    [ refactor ] library: node

--- lib/elisp2any/node.rb
+++ lib/elisp2any/node.rb
@@ -19,25 +19,26 @@ module Elisp2any
         when :comment
           scanner = StringScanner.new(content)
           scanner.skip(';') or raise Error, 'no semicolon for comment'
-          if scanner.skip(';')
-            if (level = scanner.skip(/;+/))
-              scanner.skip(' ') or raise Error, 'no space after heading semicolons'
-              nodes << Heading.new(node, level, scanner.rest.chomp)
-            elsif scanner.skip("\n")
-              # nop
-            else
-              scanner.skip(' ') or raise Error, "no space after semicolons: #{scanner.inspect}"
-              line = Line.parse(scanner.rest)
-              if (last_node = nodes.last) && last_node.is_a?(Paragraph) && last_node.end_row + 1 == top_level_node.start_point.row
-                last_node << line
-              else
-                paragraph = Paragraph.new(node, [line], top_level_node.end_point.row)
-                nodes << paragraph
-              end
-            end
-          else
+          unless scanner.skip(';')
             (last_node = nodes.last) && last_node.is_a?(CodeBlock) or raise Error, 'no prior code for single semicolon comment'
             last_node.append(source, range.end)
+            next
+          end
+
+          if (level = scanner.skip(/;+/))
+            scanner.skip(' ') or raise Error, 'no space after heading semicolons'
+            nodes << Heading.new(node, level, scanner.rest.chomp)
+          elsif scanner.skip("\n")
+            # nop
+          else
+            scanner.skip(' ') or raise Error, "no space after semicolons: #{scanner.inspect}"
+            line = Line.parse(scanner.rest)
+            if (last_node = nodes.last) && last_node.is_a?(Paragraph) && last_node.end_row + 1 == top_level_node.start_point.row
+              last_node << line
+            else
+              paragraph = Paragraph.new(node, [line], top_level_node.end_point.row)
+              nodes << paragraph
+            end
           end
         else
           if (last_node = nodes.last) && last_node.is_a?(CodeBlock)
